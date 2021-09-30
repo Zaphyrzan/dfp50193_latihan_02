8d30b3ce3849181fd1bfa344fa39da7f77503e6f
Zaphyrzan <wzafirzan@gmail.com>
2021-09-30 10:36:05 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Kedua</title>
+    
+</head>
+<body>
+    <h1>Hello World</h1>
+
+    <?php
+    $nama = 'Ahammad';
+    $umur = 20 + 3;
+
+    echo "$nama berumur $umur tahun";
+    ?>
+</body>
+</html>
